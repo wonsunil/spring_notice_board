3819e39b8a8d7d96a6cd1b8829d9e4f4ccab26f3
wonsunil <seonil6352@gmail.com>
2021-06-01 17:16:10 +0900

    Feat: content search

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -57,7 +57,9 @@ const columns = [
 ];
 
 const data = [];
+let check = false;
 let current = 0;
+let limit = 10;
 let total = 0;
 let searchIndex = -1;
 let column = "number";
@@ -78,7 +80,7 @@ let column = "number";
 
     gridView.setColumns(columns);
     gridView.setDataSource(dataProvider);
-    gridView.setPaging(true, 10);
+    gridView.setPaging(true, limit);
     total = gridView.getPageCount() - 1;
 })();
 
@@ -105,25 +107,87 @@ $next.addEventListener("click", () => {
 });
 
 const $searchColumn = document.querySelector("#search-column");
+column = $searchColumn?.value;
+
 $searchColumn.addEventListener("change", ({ target: { value } }) => {
     column = value;
+    searchIndex = -1;
 });
+
+const searchItem = (field, value, startIndex, partialMatch) => gridView.searchItem({ fields: [field], values: [value], startIndex: startIndex, partialMatch: partialMatch });
+const getColumnIndex = columnName => {
+    let index = 0;
+
+    for(let i = 0; i < fields.length; i++) {
+        if(fields[i].fieldName !== columnName) index++;
+        if(fields[i].fieldName === columnName) break;
+    }
+
+    return index;
+};
+const getPage = () => gridView.getPage();
+const setPage = page => gridView.setPage(page);
+const setCurrent = (index, column) => gridView.setCurrent({ itemIndex: searchIndex, column: column });
+const getLastIndex = text => {
+    let index = -1;
+
+    for(let i = 0; i < 10; i++) {
+        const searchedIndex = gridView.searchItem({
+            fields: [column],
+            values: [text],
+            startIndex: searchIndex + 1,
+            wrap: false,
+            partialMatch: column !== "number",
+        });
+
+        if(searchedIndex !== -1) index++;
+        if(searchedIndex === -1) break;
+    }
+
+    return index;
+};
 
 const $search = document.querySelector("#search-input");
 $search.addEventListener("keyup", ({ target: { value }, key }) => {
     if(key !== "Enter") return;
 
-    searchIndex = gridView.searchItem({
-        fields: [column],
-        values: [value],
-        startIndex: searchIndex + 1,
-        partialMatch: column === "content",
-    });
+    searchIndex = searchItem(column, value, searchIndex + 1, column !== "number");
 
+    if(getLastIndex(value) !== -1 && check) {
+        let index = -1;
+        const columnIndex = getColumnIndex(column);
+
+        for(let i = 10, limit = data.length; i < limit; i++) {
+            const item = data[i];
+
+            if(item[columnIndex].indexOf(value) !== -1) {
+                index = i;
+
+                break;
+            };
+        }
+
+        setPage(Math.floor(index / 10));
+        setCurrent(searchItem(column, value, searchIndex + 1, column !== "number"));
+
+        check = !check;
+
+        return;
+    };
+
+    if(getLastIndex(value) === -1 && !check) {
+        if(getPage() === total) {
+            setPage(0);
+
+            searchIndex = searchItem(column, value, searchIndex + 1, column !== "number"), column;
+            setCurrent(searchIndex);
+
+            return;
+        };
+
+        check = true;
+    };
     if(searchIndex < 0) return;
 
-    gridView.setCurrent({
-        itemIndex: searchIndex,
-        column: column,
-    });
+    setCurrent(searchIndex, column);
 });
